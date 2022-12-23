fbc54162bc97104770e4617c28ada598e5ade146
Ashish Kumar <ashish.kumar@credgenics.com>
2022-12-23 08:23:51 +0530

    homepage done

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,10 @@
 import react from "react";
-import DensityMediumIcon from "@mui/icons-material/DensityMedium";
+import { useState, useEffect } from "react";
 import {
-  Drawer,
-  Divider,
   Typography,
   Box,
-  Select,
-  InputLabel,
-  InputGroup,
-  MenuItem,
-  OutlinedInput,
-  FormControl,
-  styled,
-  InputBase,
-  Autocomplete,
-  TextField,
-  Collapse,
-  Checkbox,
-  FormGroup,
-  FormControlLabel,
-  Switch,
   Stack,
   Grid,
-  Paper,
-  IconButton,
-  Snackbar,
-  Modal,
-  FormHelperText,
-  Tooltip,
-  InputAdornment,
   Button,
   Card,
 } from "@mui/material";
@@ -39,69 +15,96 @@ import TimerIcon from "@mui/icons-material/Timer";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import SpeedIcon from "@mui/icons-material/Speed";
 import DragHandleIcon from "@mui/icons-material/DragHandle";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { useNavigate } from "react-router-dom";
+import Chart from './Chart'
+import { getLoans } from "./service";
+import uniqolor from 'uniqolor';
+
 
 const Home = () => {
-  const navigate = useNavigate();
-  const LoanCard = () => (
-    <Grid
-      container
-      sx={{ background: "#FAFAFA", border: "1px solid #DDDDDD", borderRadius: "4px", mt: 1 }}
-    >
-      <Grid item xs={12}>
+  const userDetails = JSON.parse(localStorage.getItem('user'));
+  const [loanDetails, setLoanDetails] = useState([])
+  const [cibilScore, setCibilScore] = useState()
+  const [totalLoans, setTotalLoans] = useState()
+  const [totalOutstanding, setTotalOutstanding] = useState()
+  const [emiOverdue, setEmiOverdue] = useState()
+  const [pieChartData, setPieChartData] = useState({})
+
+  const colors = ["#36cfc9",
+  "#38c3d0",
+  "#3bb2d1",
+  "#3da1d3",
+  "#4090d4",
+  "#4280d5",
+  "#4570d7",
+  "#4760d8",
+  "#4a50d9",
+  "#574cda",
+  "#6b4fdb",
+  "#7f51dd",
+  "#9254de"
+]
+
+  useEffect(()=>console.log(colors), [])
+
+
+  useEffect(() => { loadPage(userDetails["id"]) }, [])
+
+  async function loadPage(id) {
+    const loanData = await getLoans(id);
+    setLoanDetails(loanData["data"]);
+    setCibilScore(loanData["cibil_score"])
+    setTotalLoans(loanData["data"].length)
+    setTotalOutstanding(loanData["total_outstanding_amount"])
+    setEmiOverdue(loanData["total_emi_amount"])
+    setPieChartData(loanData["product_type_pie_chart_data"])
+  }
+  const impact = (value) => {
+    if (value === "high") return (<ArrowUpwardIcon sx={{ color: "red" }} />)
+    if (value === "low") return (<ArrowDownwardIcon sx={{ color: "green" }} />)
+    if (value === "medium") return (<DragHandleIcon sx={{ fontSize: { sm: "14px", xs: "10px" } }} />)
+  }
+  const LoanCard = (loan) => {
+    const loanInfo = loan.loan; return (
+      <Grid
+        container
+        sx={{ background: "#FAFAFA", border: "1px solid #DDDDDD", borderRadius: "4px", mt: 1 }}
+      >
         <Stack sx={{}}>
-          <Grid container>
-            <Grid xs={11}>
-              <Box sx={{ pt: 1 }}>
-                <Typography
-                  sx={{
-                    fontWeight: 500,
-                    fontSize: "14px",
-                    lineHeight: "18px",
-                    color: "#535353",
-                    px: 1,
-                  }}
-                >
-                  Rakshit Walia
-                </Typography>
-              </Box>
-              <Box>
-                <Typography
-                  sx={{
-                    fontSize: "14px",
-                    px: 1,
-                  }}
-                >
-                  <span style={{ color: "rgba(83, 83, 83, 0.75)", fontWeight: 400 }}>
-                    Amount Due:
-                  </span>
-                  <span style={{ color: "#00000", fontWeight: 600 }}>₹1,50,0000</span>
-                </Typography>
-              </Box>
-              <Box>
-                <Typography
-                  sx={{
-                    fontSize: "14px",
-                    px: 1,
-                  }}
-                >
-                  <span style={{ color: "rgba(83, 83, 83, 0.75)", fontWeight: 400 }}>Loan:</span>
-                  <span style={{ color: "#00000", fontWeight: 600 }}>Car Loan</span>
-                </Typography>
-              </Box>
-            </Grid>
-
-            <Grid item xs={1} sx={{}}>
-              <Box
-                sx={{ cursor: "pointer", float: "right", p: 1 }}
-                onClick={() => navigate("/loan-details")}
-              >
-                <ArrowForwardIosIcon />
-              </Box>
-            </Grid>
-          </Grid>
-
+          <Box sx={{ pt: 1 }}>
+            <Typography
+              sx={{
+                fontWeight: 500,
+                fontSize: "14px",
+                lineHeight: "18px",
+                color: "#535353",
+                px: 1,
+              }}
+            >
+              {loanInfo["company_name"].toUpperCase()}
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              sx={{
+                fontSize: "14px",
+                px: 1,
+              }}
+            >
+              <span style={{ color: "rgba(83, 83, 83, 0.75)", fontWeight: 400 }}>Amount Due:</span>
+              <span style={{ color: "#00000", fontWeight: 600 }}>₹{loanInfo["emi_amount"]}</span>
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              sx={{
+                fontSize: "14px",
+                px: 1,
+              }}
+            >
+              <span style={{ color: "rgba(83, 83, 83, 0.75)", fontWeight: 400 }}>Loan:</span>
+              <span style={{ color: "#00000", fontWeight: 600 }}>{loanInfo["loan_type"]}</span>
+            </Typography>
+          </Box>
           <Stack direction={"row"} sx={{ my: 1 }}>
             <Box>
               <Button
@@ -115,7 +118,7 @@ const Home = () => {
                   fontSize: { sm: "14px", xs: "10px" },
                 }}
                 startIcon={<CurrencyRupeeIcon sx={{ fontWeight: 600, color: "#585858" }} />}
-                endIcon={<ArrowUpwardIcon sx={{ color: "red" }} />}
+                endIcon={impact(loanInfo["money_impact"])}
               >
                 Money Impact
               </Button>
@@ -132,7 +135,7 @@ const Home = () => {
                   fontSize: { sm: "14px", xs: "10px" },
                 }}
                 startIcon={<TimerIcon sx={{ color: "#585858" }} />}
-                endIcon={<ArrowDownwardIcon sx={{ color: "green" }} />}
+                endIcon={impact(loanInfo["urgency"])}
               >
                 Urgency
               </Button>
@@ -152,21 +155,22 @@ const Home = () => {
                 startIcon={
                   <SpeedIcon sx={{ color: "#585858", fontSize: { sm: "14px", xs: "10px" } }} />
                 }
-                endIcon={<DragHandleIcon sx={{ fontSize: { sm: "14px", xs: "10px" } }} />}
+                endIcon={impact(loanInfo["cibil_impact"])}
               >
                 CIBIL
               </Button>
             </Box>
           </Stack>
+          <Box></Box>
         </Stack>
       </Grid>
-    </Grid>
-  );
+    );
+  }
   return (
-    <Grid continer sx={{ mt: { sm: 2, xs: -2 }, mx: 2 }}>
+    <Grid container sx={{ mt: { sm: 2, xs: -2 }, mx: 2 }}>
       <Grid item xs={12}>
         <Typography sx={{ fontWeight: 500, fontSize: "20px" }} color="#4164AB">
-          Rakshit
+          {userDetails["user_name"]}
         </Typography>
       </Grid>
 
@@ -174,14 +178,14 @@ const Home = () => {
         <Grid container sx={{ backgroundColor: "#F3F3F3", pb: 1 }}>
           <Grid item xs={12} sx={{ px: 2, py: 1, backgroundColor: "#fff" }}>
             <Typography sx={{ fontWeight: 500, fontSize: "16px" }} color="#00000">
-              CIBIL SCORE - 868
+              CIBIL SCORE - {cibilScore}
             </Typography>
           </Grid>
           <Grid xs={7} sx={{ mt: 1 }}>
             <GaugeChart
               style={{ marginLeft: "-28px" }}
               id="gauge-chart6"
-              percent={0.9}
+              percent={cibilScore / 1300}
               nrOfLevels={5}
               needleBaseColor="#000"
               cornerRadius={0}
@@ -273,7 +277,7 @@ const Home = () => {
               }}
             >
               <Typography sx={{ textAlign: "center" }}>Total Loans</Typography>
-              <Typography>5</Typography>
+              <Typography>{totalLoans}</Typography>
             </Box>
           </Grid>
           <Grid item xs={6}>
@@ -295,7 +299,7 @@ const Home = () => {
                   color: "#212121",
                 }}
               >
-                $1,000,000
+                ₹{totalOutstanding}
               </Typography>
             </Box>
           </Grid>
@@ -313,73 +317,34 @@ const Home = () => {
                   color: "#212121",
                 }}
               >
-                $1,000,000
+                ₹{emiOverdue}
               </span>
             </Typography>
           </Grid>
-          <Grid xs={7} sx={{ mt: 1 }}></Grid>
-          <Grid item xs={5} sx={{ mt: 1 }}>
-            <Box sx={{ display: "flex", mt: { sm: 1, xs: 0.5 } }}>
+          <Grid xs={7} sx={{ mt: 1 }}>
+            <Box sx={{ ml: 3, mb: 1, width: 120 }}>
+              <Chart pieChartData={pieChartData} />
+            </Box>
+          </Grid>
+          <Grid item xs={5} sx={{ mt: 2 }}>
+            {Object.keys(pieChartData).map((loan_type, index)=>(<Box sx={{ display: "flex", mt: { sm: 1, xs: 0.5 } }}>
               {" "}
               <div
                 style={{
                   width: "12px",
                   height: "12px",
                   borderRadius: "3px",
-                  backgroundColor: "#40A9FF",
+                  backgroundColor: colors[index],
                   margin: "4px 5px",
                 }}
               ></div>
-              <span>Home Loan</span>
-            </Box>
-            <Box sx={{ display: "flex", mt: { sm: 1, xs: 0.5 } }}>
-              {" "}
-              <div
-                style={{
-                  width: "12px",
-                  height: "12px",
-                  borderRadius: "3px",
-                  backgroundColor: "#36CFC9",
-                  margin: "4px 5px",
-                }}
-              ></div>
-              <span>Gold Loan</span>
-            </Box>
-            <Box sx={{ display: "flex", mt: { sm: 1, xs: 0.5 } }}>
-              {" "}
-              <div
-                style={{
-                  width: "12px",
-                  height: "12px",
-                  borderRadius: "3px",
-                  backgroundColor: "#597EF7",
-                  margin: "4px 5px",
-                }}
-              ></div>
-              <span>Personal Loan</span>
-            </Box>
-            <Box sx={{ display: "flex", mt: { sm: 1, xs: 0.5 } }}>
-              {" "}
-              <div
-                style={{
-                  width: "12px",
-                  height: "12px",
-                  borderRadius: "3px",
-                  backgroundColor: "#9254DE",
-                  margin: "4px 5px",
-                }}
-              ></div>
-              <span>Car Loan</span>
-            </Box>
+              <span>{loan_type.toUpperCase()}</span>
+            </Box>))}
           </Grid>
         </Grid>
         <Grid container>
           <Grid item xs={12} sx={{ p: 1 }}>
-            <LoanCard />
-
-            <LoanCard />
-            <LoanCard />
-            <LoanCard />
+            {loanDetails.map((loan) => { return (<LoanCard loan={loan} />) })}
           </Grid>
         </Grid>
       </Card>
