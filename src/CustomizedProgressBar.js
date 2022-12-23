2006f1dbfdb78badf71466d5e96e74cfc711803d
Ashish Kumar <ashish.kumar@credgenics.com>
2022-12-23 11:16:21 +0530

    progress bar

---
+++ src/CustomizedProgressBar.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: '#A7D5FF',
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+  },
+}));
+
+export default function CustomizedProgressBar({value}) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <BorderLinearProgress variant="determinate" value={value} />
+    </Box>
+  );
+}
--- src/Settlement.js
+++ src/Settlement.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import CustomizedProgressBar from './CustomizedProgressBar'
 const Settlement = () => {
   return (
-    <div>Settlement</div>
+    <div><CustomizedProgressBar value={90}/></div>
   )
 }
 
